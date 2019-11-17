f9f48a3578bc6ed3c64bdf94769a51793a5a3500
Rivindu Wijedoru <rivindu@outlook.com>
2019-11-16 19:47:33 -0500

    added example server and client files

---
+++ catkin_ws/src/front_expo/src/ex_client.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+import roslib
+roslib.load_manifest('my_pkg_name')
+import rospy
+import actionlib
+
+from chores.msg import DoDishesAction, DoDishesGoal
+
+if __name__ == '__main__':
+    rospy.init_node('do_dishes_client')
+    client = actionlib.SimpleActionClient('do_dishes', DoDishesAction)
+    client.wait_for_server()
+
+    goal = DoDishesGoal()
+    # Fill in the goal here
+    client.send_goal(goal)
+    client.wait_for_result(rospy.Duration.from_sec(5.0))
